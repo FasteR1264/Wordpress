1d236c057523c994a23894178710734c3c92e4b0
Andrei <example@gmail.com>
2022-06-22 14:39:13 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
+
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('styles', function(){
+    return  gulp.src('./sass/*.+(scss|sass)')
+            .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+            .pipe(rename({
+                prefix: "",
+                suffix: ".min",
+            }))
+		    .pipe(autoprefixer({
+			    cascade: false
+		    }))
+            .pipe(cleanCSS({compatibility: 'ie8'}))
+            .pipe(gulp.dest('./css'))
+            .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function(){
+    gulp.watch("./sass/*.+(sccc|sass)", gulp.parallel('styles'));
+    gulp.watch("./*.html").on("change", browserSync.reload);
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles' ));
